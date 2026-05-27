e85db213b6bfe6a3862c97ea2b2c65a0c70b2937
nandarina7PNU <nandarina7@pusan.ac.kr>
2026-05-28 01:30:01 +0900

    테스트 커밋입니다.

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
             다시 만나서 반갑습니다
           </h1>
           <p className="loginDescription">
-            이메일과 비밀번호를 입력해 로그인하세요. 테스트 계정은 <strong>test@example.com / password123</strong>입니다.
+            이메일과 비밀번호를 입력해 로그인하세요!!!! 테스트 계정은 <strong>test@example.com / password123</strong>입니다.
           </p>
 
           <LoginForm />
