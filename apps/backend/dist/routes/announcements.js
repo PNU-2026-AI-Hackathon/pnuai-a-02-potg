f54bcc1255734b187fb385c7e520dfa3c3172dd4
nandarina7PNU <nandarina7@pusan.ac.kr>
2026-06-26 15:19:42 +0900

    chore : 백엔드 생성 파일 추적 제거

--- apps/backend/dist/routes/announcements.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const mockData_1 = require("../data/mockData");
-const router = (0, express_1.Router)();
-router.get('/', (_req, res) => {
-    res.json(mockData_1.announcements);
-});
-exports.default = router;
